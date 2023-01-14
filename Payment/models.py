61cc9d7f1ccba5a309174eedb4b0caa9a68e0614
k-roozbahani <kavehrozbah@gmail.com>
2023-01-14 16:45:31 +0330

    kaveh order

--- Payment/models.py
+++ Payment/models.py
@@ -1,6 +1,9 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 from User.models import User
+from Product.models import Product
 
 
 # Cart and Cart Item fields must be similar to Order and Order item
@@ -21,14 +24,60 @@ class Order(models.Model):
     PAYMENT_CREDIT = 3
     PAYMENT_TYPE = ((PAYMENT_ONLINE, _('online')), (PAYMENT_CASH, _('cash')), (PAYMENT_CREDIT, _('credit')))
     status = models.PositiveIntegerField(verbose_name=_('status'), default=1)
-    taking_code = models.PositiveIntegerField(verbose_name=_('tracking code'), auto_created=True, unique=True)
-    order_price = models.DecimalField(verbose_name=_('order price'), max_digits=5, default=0)
+    tracking_code = models.PositiveIntegerField(verbose_name=_('tracking code'), auto_created=True, unique=True)
+    order_price = models.PositiveBigIntegerField(verbose_name=_('order price'), default=0)
     payment_type = models.PositiveSmallIntegerField(verbose_name=_('payment type'), choices=PAYMENT_TYPE, default=1)
+    created_time = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
+    modified_time = models.DateTimeField(verbose_name=_('modified time'), auto_now=True)
+
+    class Meta:
+        db_table = 'order'
+        verbose_name = 'order'
+        verbose_name_plural = 'orders'
 
 
 class OrderItem(models.Model):
-    pass
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_item")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="order_items")
+    expire_time = models.DateTimeField(default=timezone.now() + timezone.timedelta(hours=1))
+    count = models.PositiveSmallIntegerField(default=0)
+    created_time = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
+    modified_time = models.DateTimeField(verbose_name=_('modified time'), auto_now=True)
+
+    class Meta:
+        db_table = 'order_item'
+        verbose_name = 'order item'
+        verbose_name_plural = 'order items'
+
+
+class OrderSend(models.Model):
+    NORMAL_POST = 1
+    FAST_POST = 2
+    POST_TYPE = ((NORMAL_POST, _('normal')), (FAST_POST, _('fast')))
+
+    order = models.ForeignKey(Order, models.CASCADE, related_name='order_send')
+    post_type = models.PositiveSmallIntegerField(verbose_name=_('post type'), default=1)
+    recipient_first_name = models.CharField(verbose_name=_('recipient first name'), max_length=64)
+    recipient_last_name = models.CharField(verbose_name=_('recipient last name'), max_length=64)
+    recipient_phone_number = models.PositiveBigIntegerField(verbose_name=_('recipient phone number'),
+                                                            validators=[RegexValidator(
+                                                                r'^989[0-3,9]\d{8}$', 'Enter a valid phone number.',
+                                                                'invalid')]
+                                                            )
+    address = models.TextField(verbose_name=_('address'))
+    send_cost = models.PositiveBigIntegerField(verbose_name=_('send cost'), default=0)
+    created_time = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True)
+    modified_time = models.DateTimeField(verbose_name=_('modified time'), auto_now=True)
+    tracking_code = models.PositiveIntegerField(verbose_name=_('tracking code'), auto_created=True, unique=True)
+
+    class Meta:
+        db_table = 'order_ send'
+        verbose_name = 'order send'
+        verbose_name_plural = 'order sends'
 
 
 class Payment(models.Model):
     pass
+    # phone_number = models.PositiveBigIntegerField(unique=True, validators=[
+    #         RegexValidator(r'^989[0-3,9]\d{8}$', 'Enter a valid phone number.', 'invalid')
+    # ])

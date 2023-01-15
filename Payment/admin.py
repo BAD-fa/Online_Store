bff7658dfa4cb32bc6128e34534d026518577496
k-roozbahani <kavehrozbah@gmail.com>
2023-01-15 12:10:16 +0330

    add admin orders

--- Payment/admin.py
+++ Payment/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import OrderSend, OrderItem, Order
 
-# Register your models here.
+
+class OrderSendTabularInline(admin.TabularInline):
+    model = OrderSend
+    fields = ('post_type', 'recipient_first_name', 'recipient_last_name', 'recipient_phone_number',
+              'address', 'send_cost', 'created_time', 'modified_time', 'tracking_code')
+
+
+extra = 0
+
+
+class OrderItemTabularInline(admin.TabularInline):
+    model = OrderItem
+    fields = ('product', 'expire_time', 'count', 'created_time', 'modified_time')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = (OrderItemTabularInline, OrderSendTabularInline)

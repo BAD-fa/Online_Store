05a33b7a01c045c7d4132a8f0c3f8a4b789dad7f
k-roozbahani <kavehrozbah@gmail.com>
2023-01-16 17:39:09 +0330

    fixed order.admin

--- Payment/admin.py
+++ Payment/admin.py
@@ -5,7 +5,7 @@ from .models import OrderSend, OrderItem, Order
 class OrderSendTabularInline(admin.TabularInline):
     model = OrderSend
     fields = ('post_type', 'recipient_first_name', 'recipient_last_name', 'recipient_phone_number',
-              'address', 'send_cost', 'created_time', 'modified_time', 'tracking_code')
+              'address', 'send_cost', 'tracking_code')
 
 
 extra = 0
@@ -13,9 +13,10 @@ extra = 0
 
 class OrderItemTabularInline(admin.TabularInline):
     model = OrderItem
-    fields = ('product', 'expire_time', 'count', 'created_time', 'modified_time')
+    fields = ('product', 'expire_time', 'count')
 
 
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     model = Order
     inlines = (OrderItemTabularInline, OrderSendTabularInline)

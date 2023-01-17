e7d2d2913ad8552ed79b79940f1aafab3a76b152
k-roozbahani <kavehrozbah@gmail.com>
2023-01-17 14:09:14 +0330

    add checkout url

--- Payment/urls.py
+++ Payment/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import OrderView
+from .views import OrderView, CheckoutView
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'order', OrderView, basename='order')
-
-urlpatterns = []
-urlpatterns += router.urls
+router.register(r'order-checkout', CheckoutView, basename='checkout')
+urlpatterns = router.urls

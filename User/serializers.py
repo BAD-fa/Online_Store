25d380aaf3e1dde42f240e56020b087b0cb45477
n-vgh98 <nazi.gh988@gmail.com>
2023-01-17 19:24:16 +0330

    Add Serilizer and Urls for profile

---
+++ User/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import *
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ('province', 'city', 'address_detail', 'postal_code', 'receiver_name', 'receiver_phone_number')
+
+class ProfileSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'

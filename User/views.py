2c02aeb359da4f43e3093f558e64b63d34326ac6
n-vgh98 <nazi.gh988@gmail.com>
2023-02-03 15:16:20 +0330

    Add permisstion and authentication class in views

--- User/views.py
+++ User/views.py
@@ -1,20 +1,21 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .serializers import *
 from .models import *
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 
 BASE_DIR = 'http://127.0.0.1:8000/profile/'
 
 
 class ProfileUrl(generics.RetrieveAPIView):
+    # authentication_classes = (JWTAuthentication,)
+    # permission_classes = [IsAuthenticated]
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
 
     def get(self, request, *args, **kwargs):
-        # profile = TODO show user profile that user login
         context = {
             'Address': BASE_DIR + 'address',
             'Information': BASE_DIR + 'information/<int:pk>',
@@ -25,11 +26,15 @@ class ProfileUrl(generics.RetrieveAPIView):
 
 
 class UserAddress(generics.ListCreateAPIView):
+    # authentication_classes = (JWTAuthentication,)
+    # permission_classes = [IsAuthenticated]
     serializer_class = AddressSerializer
     queryset = Address.objects.filter(is_valid=True)
 
 
 class CustomerInfo(generics.RetrieveUpdateDestroyAPIView):
+    # authentication_classes = (JWTAuthentication,)
+    # permission_classes = [IsAuthenticated]
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
 
